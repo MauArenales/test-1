6961d66443df2f74d811c70bb9705b468fc663d1
mauricio <arenales.mauricio24@gmail.com>
2024-01-14 16:58:05 -0500

    segundo commit

--- first/src/main/Principal.java
+++ first/src/main/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
     public static void main(String[] args){
         System.out.println("Hola mundo");
         System.out.println("Cambio insertado");
+        System.out.println("Cambio insertado 2");
     }
 }
