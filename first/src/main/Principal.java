c268e7a4e5635ff9a1655dfed7c0f4def4024370
mauricio <arenales.mauricio24@gmail.com>
2024-01-14 21:04:49 -0500

    commit 1 from NB

--- first/src/main/Principal.java
+++ first/src/main/Principal.java
@@ -4,8 +4,6 @@ package main;
 public class Principal {
     public static void main(String[] args){
         System.out.println("Hola mundo");
-        System.out.println("Cambio insertado");
-        System.out.println("Cambio insertado 2");
-        System.out.println("Cambio insertado 4");
+
     }
 }
